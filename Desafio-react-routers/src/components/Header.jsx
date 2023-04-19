9e6c9f5f9b25438b121ac4bb8978e50d1a952261
wendel <wh68639@gmail.com>
2023-04-19 11:38:22 -0300

    ok

--- Desafio-react-routers/src/components/Header.jsx
+++ Desafio-react-routers/src/components/Header.jsx
@@ -1,17 +1,23 @@
-import Logo from '../assets/imagens/viagens.jpg';
+import Logo from '../assets/imagens/viagens.jpg'
+import * as React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 function Header() {
     return (
-        <div className='flex justify-around'>
-            <img src={Logo} />
+        <div className='flex justify-center gap-14 items-center'>
+            <img src={Logo} className='w-20' />
 
-            <p>Home</p>
+            <Link to=".." relative="path">Home</Link>
             <p>Grand Canyon</p>
             <p>Escócia</p>
             <p>Muralhas da China</p>
             <p>Aruba</p>
 
-            <input type='text'className='' />
+            <input type='text'className='border'  />
+
+            <FontAwesomeIcon icon={faMagnifyingGlass} className='w-4' />
         </div>
     )
 }
