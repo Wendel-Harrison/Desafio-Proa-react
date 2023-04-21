a1f24343050e160bd5aad2f81a6f5f40964d1ae8
wendel <wh68639@gmail.com>
2023-04-21 19:54:29 -0300

    Aruba

--- Desafio-react-routers/src/pages/Aruba.jsx
+++ Desafio-react-routers/src/pages/Aruba.jsx
@@ -3,6 +3,13 @@ import Details from '../components/Details'
 
 const Aruba = () => {
   return (
+    <div className={`bg-aruba w-full h-[550px] bg-cover pt-14 `}>
+    
+            <Details
+              title="Aruba" 
+              details="Praias paradisíacas, mar cristalino, hotéis requintados, cassinos e programação para toda a família são alguns dos atrativos de Aruba." 
+            />
+        </div>
     
   )
 }
