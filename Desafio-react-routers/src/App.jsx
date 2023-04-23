e8dd8ae647eb27a02764ea4b762043d394587194
wendel <wh68639@gmail.com>
2023-04-23 13:46:01 -0300

    teste routers

--- Desafio-react-routers/src/App.jsx
+++ Desafio-react-routers/src/App.jsx
@@ -1,15 +1,26 @@
 
 import Footer from "./components/Footer"
 import Header from "./components/Header"
+import Aruba from "./pages/Aruba"
+import Home from "./pages/Home"
 
-import { Outlet } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
+import Canyon from "./pages/Canyon"
+import China from "./pages/China"
+import Escocia from "./pages/Escocia"
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/aruba" element={<Aruba/>} />
+        <Route path="/canyon" element={<Canyon/>} />
+        <Route path="/china" element={<China/>} />
+        <Route path="/escocia" element={<Escocia/>} />
+      </Routes>
       <Footer />
     </div>
   )
--- Desafio-react-routers/src/main.jsx
+++ Desafio-react-routers/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 //1 configurando o router 
 
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Canyon from './pages/Canyon'
 import Error from './pages/ErrorPage'
@@ -13,42 +13,12 @@ import Escocia from './pages/Escocia'
 import China from './pages/China'
 import Aruba from './pages/Aruba'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: '/',
-        element: <Home />
-      },
-      {
-        path: '/canyon',
-        element: <Canyon />
-      },
-      {
-        path: '/escocia',
-        element: <Escocia />
-      },
-      {
-        path: '/china',
-        element: <China />
-      },
-      {
-        path: '/aruba',
-        element: <Aruba />
-      }
-      // nested routes = identificador unico
-      
-    ]
-  },
-  
-])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
     
   </React.StrictMode>,
 )
